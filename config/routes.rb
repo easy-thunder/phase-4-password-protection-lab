6ea01409a3ae19efc18cb9125831866c050d9da2
Jacob David Diehl <jakediehl17@gmail.com>
2023-02-28 14:20:47 -0700

    commit tree e6827a0506bd726480c98047a0ad9ac45fecd06c

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  # resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show"
+  delete "/logout", to: "sessions#destroy"
+  post "/login", to: "sessions#create"
+
+
 end
